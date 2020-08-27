c26a2c15d860e085b454a2adcafadcce6da82f06
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-08-27 04:13:13 +0300

    simple retrofit app

---
+++ app/src/main/java/kapadokia/nyandoro/retrofit/apiInterfaces/APIInterface.java
@@ -0,0 +1,29 @@
+package kapadokia.nyandoro.retrofit.apiInterfaces;
+
+
+import kapadokia.nyandoro.retrofit.model.MultipleResource;
+import kapadokia.nyandoro.retrofit.model.User;
+import kapadokia.nyandoro.retrofit.model.UserList;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
---
+++ app/src/main/java/kapadokia/nyandoro/retrofit/model/CreateUserResponse.java
@@ -0,0 +1,16 @@
+package kapadokia.nyandoro.retrofit.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CreateUserResponse {
+
+    @SerializedName("name")
+    public String name;
+    @SerializedName("job")
+    public String job;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("createdAt")
+    public String createdAt;
+}
